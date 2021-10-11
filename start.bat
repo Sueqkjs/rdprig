b4d27ccb49f63f650164f044848011f03ef6a34a
Bread <bread@theradservices.onmicrosoft.com>
2021-10-11 16:44:50 +0900

    Re

--- start.bat
+++ start.bat
@@ -3,13 +3,20 @@
 net session >nul 2>&1
 
 if %ERRORLEVEL% equ 0 (
-  echo processing....
-  winget install git
-  git clone https://github.com/Sueqkjs/rdprig
+  echo processing...
+  mkdir rdprig
   cd rdprig
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/xmrig.exe -UseBasicParsing -OutFile xmrig.exe
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/xmrig-asm.lib -UseBasicParsing -OutFile xmrig-asm.lib
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/WinRing0x64.sys -UseBasicParsing -OutFile WinRing0x64.sys
+  
+  :: Change this!
+  :: https://raw.githubusercontent.com/[Your GitHub username]/rdprig/master/config.json
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/config.json -UseBasicParsing -OutFile config.json
+  
   xmrig.exe
 ) else (
-  echo kanrisya de zikkou siro
+  echo kanrisya de zikkou siro hage
 )
 
 pause
